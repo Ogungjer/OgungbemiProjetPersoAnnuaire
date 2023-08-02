57e8cdf87eb99e42a457ffc16ee2bf779825b260
Ogungjer <ogungbemi03@gmail.com>
2023-08-02 15:23:50 +0200

    feat(controller,dataContext,models,service,migration) : add-datacontext, crud-site

---
+++ ProjetPersoAnnuaire/Services/SitesService/ISiteService.cs
@@ -0,0 +1,18 @@
+﻿using ProjetPersoAnnuaire.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace ProjetPersoAnnuaire.Services.SitesService
+
+{
+    public interface ISiteService
+    {
+        Task<IEnumerable<Site>> GetAllSites();
+        Task<Site> GetSiteById(int id);
+        Task<int> AddSite(Site hero);
+        Task<int> UpdateSite(Site hero);
+        Task<int> DeleteSite(int id);
+
+    }
+}
